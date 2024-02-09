7076138986eb2d9710e51b4063345c78ec51a548
Krzysztof Wolicki <der.teufel.mail@gmail.com>
2024-02-09 13:31:30 +0000

    Fix build.zig

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     _ = b.addModule("treez", .{
-        .source_file = .{ .path = "treez.zig" },
+        .root_source_file = .{ .path = "treez.zig" },
     });
 
     const target = b.standardTargetOptions(.{});
